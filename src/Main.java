f33313c1fdcc5d7e8a5e6c10e7e89684b0cbf9fc
MaximShev <max.93@list.ru>
2023-11-09 01:23:05 +0600

    сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Задаем изначальные значения переменных
         var productPrice = 7990.0;
-        var goal = 200000.0;
+        var goal = 2000000.0;
         var tax = 0.13;
 // Рассчитываем количество экземпляров, которые нужно продать
         var toSale = goal / (productPrice * (1.0 - tax));
